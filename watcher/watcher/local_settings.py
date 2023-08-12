e71488866734c617ceaa5aa13c42ce6af85ded3f
AmiR <zahedi.1372@gmail.com>
2023-08-12 12:15:23 +0330

    initial commit

---
+++ watcher/watcher/local_settings.py
@@ -0,0 +1,13 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-*nblc52-=zv1$716%e=d33en08ebf*@7)smeet0(h3t+k3v!jg'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+DB = {
+    'NAME' : 'watcher',
+    'USER' : 'postgres',
+    'PASS' : 'old_snake1993',
+    'HOST' : 'localhost',
+    'PORT' : '5432',
+}
